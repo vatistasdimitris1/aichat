db66d8652174824cb1ba4649606c7f490f3bacd0
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-12 00:03:42 +0300

    css new

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () 
     const chatBox = document.getElementById("chat-box");
     const userInput = document.getElementById("user-input");
     const sendButton = document.getElementById("send-button");
