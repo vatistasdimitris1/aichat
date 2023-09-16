4b5053966f29ee0cce57757b89857dbfe42b65a1
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-16 19:39:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,25 +5,26 @@ document.addEventListener("DOMContentLoaded", function () {
     nextButton.addEventListener("click", function () {
         const email = emailInput.value.trim();
         if (isValidEmail(email)) {
-            // Check payment status using info.js
-            const hasPaid = checkPaymentStatus(email);
-
-            if (hasPaid) {
-                alert("You have already paid. You can enter the chat.");
-                // Redirect to the chat.html page or any other action you want
-                window.location.href = "main.html"; // Replace with the desired URL
+            if (isAdminEmail(email)) {
+                // Redirect to main.html if the email is the admin's email
+                window.location.href = "main.html";
             } else {
-                alert("You have not paid yet. Please make a payment.");
-                // Redirect to the payment page or any other action you want
-                window.location.href = "pay.html"; // Replace with the desired URL
+                // Handle non-admin user actions or validations
+                alert("Welcome non-admin user.");
+                // Perform other actions for non-admin users
             }
         } else {
             alert("Invalid email address. Please enter a valid email.");
         }
     });
 
-    // Function to validate an email address (you can use a more robust validation method)
+    // Function to validate an email address
     function isValidEmail(email) {
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    }
+
+    // Function to check if the email is the admin's email
+    function isAdminEmail(email) {
+        return email === "familyvatistas90@gmail.com"; // Replace with your admin's email
     }
 });
