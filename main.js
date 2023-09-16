5b13670abdf231437b2c6448193b6ca441811e35
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-16 15:02:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-// Import functions from info.js
-import { saveUserInfo, getUserInfo } from './info.js';
+// Function to save user information to localStorage
+function saveUserInfo(email, password) {
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+}
 
-// Function to handle login
-document.querySelector("#login-form form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const loginEmail = document.getElementById("login-email").value;
-    const loginPassword = document.getElementById("login-password").value;
-
-    // Check if the email and password match your stored credentials
-    // For simplicity, I'm using a hardcoded email and password.
-    if (loginEmail === "your@email.com" && loginPassword === "yourpassword") {
-        // Save user information to localStorage
-        saveUserInfo(loginEmail, loginPassword);
-
-        // Redirect to price-plans.html
-        window.location.href = "price-plans.html";
-    } else {
-        alert("Invalid email or password. Please try again.");
-    }
-});
+// Function to get user information from localStorage
+function getUserInfo() {
+    const email = localStorage.getItem("email");
+    const password = localStorage.getItem("password");
+    return { email, password };
+}
 
 // Function to handle sign-up
 document.querySelector("#signup-form form").addEventListener("submit", function (e) {
@@ -29,6 +20,28 @@ document.querySelector("#signup-form form").addEventListener("submit", function 
     // Save user information to localStorage
     saveUserInfo(signupEmail, signupPassword);
 
-    // Redirect to price-plans.html
-    window.location.href = "price-plans.html";
+    // Clear the sign-up form
+    document.getElementById("signup-email").value = "";
+    document.getElementById("signup-password").value = "";
+
+    // Show a message to prompt the user to log in
+    const messageDiv = document.createElement("div");
+    messageDiv.classList.add("message");
+    messageDiv.textContent = "Account created! Please log in.";
+    document.querySelector(".buttons").appendChild(messageDiv);
+
+    // Hide the sign-up form
+    document.getElementById("signup-form").classList.add("hidden");
+});
+
+// Function to display the login form when the "Login" button is clicked
+document.getElementById("login-button").addEventListener("click", function () {
+    // Hide the message
+    const messageDiv = document.querySelector(".message");
+    if (messageDiv) {
+        messageDiv.style.display = "none";
+    }
+
+    // Show the login form
+    document.getElementById("login-form").classList.remove("hidden");
 });
