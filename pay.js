b475c72ed0d02e228d616e2f0cb67478f26ceabd
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-16 23:12:55 +0300

    Update pay.js

--- pay.js
+++ pay.js
@@ -1,22 +1,26 @@
-paypal.Buttons({
-    createOrder: function(data, actions) {
-        // This function sets up the details of the transaction, including the amount and currency.
-        return actions.order.create({
-            purchase_units: [{
-                amount: {
-                    value: '50.00' // One-time payment amount (adjust as needed)
-                }
-            }]
-        });
-    },
-    onApprove: function(data, actions) {
-        // This function captures the funds from the transaction.
-        return actions.order.capture().then(function(details) {
-            // After a successful payment, you can redirect the user to another page.
-            alert('Transaction completed by ' + details.payer.name.given_name);
-            // Save email and payment status to info.js (you need to implement this)
-            // Redirect to the index.html page
-            window.location.href = "main.html";
-        });
-    }
-}).render('#paypal-button-container');
+import { saveEmailAndPaymentStatus } from './info.js';
+
+document.addEventListener("DOMContentLoaded", function () {
+    const userEmailInput = document.getElementById("user-email");
+    const payButton = document.getElementById("pay-button");
+
+    paypal.Buttons({
+        createOrder: function (data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: '50.00'
+                    }
+                }]
+            });
+        },
+        onApprove: function (data, actions) {
+            return actions.order.capture().then(function (details) {
+                const userEmail = userEmailInput.value.trim();
+                saveEmailAndPaymentStatus(userEmail, true);
+
+                window.location.href = "main.html";
+            });
+        }
+    }).render('#paypal-button-container');
+});
