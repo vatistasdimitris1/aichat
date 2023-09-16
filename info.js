1ee9dd449aefb97a86974bcff8ba7f990159f20c
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-16 20:09:01 +0300

    Update info.js

--- info.js
+++ info.js
@@ -1,16 +1,11 @@
 // Sample user information (replace with your actual user data)
 const users = [
-    { email: 'familyvatistas90@gmail.com', isAdmin: true, hasPaid: true },
-    { email: 'user2@example.com', isAdmin: false, hasPaid: true },
+    { email: 'user@example.com', isAdmin: true, hasPaid: true },
+    { email: 'user2@example.com', isAdmin: false, hasPaid: false },
     // Add more user data as needed
 ];
 
 // Function to check user credentials and return user data
-function getUserData(email) {
+export function getUserData(email) {
     return users.find(user => user.email === email);
 }
-
-// Export the getUserData function (you can add more functions as needed)
-module.exports = {
-    getUserData
-};
