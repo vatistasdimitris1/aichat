df3c8224ab21dd221285f8fbb4fef713bb309b14
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-16 20:08:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+// Import the getUserData function from info.js
+import { getUserData } from './info.js';
+
 document.addEventListener("DOMContentLoaded", function () {
     const nextButton = document.getElementById("next-button");
     const emailInput = document.getElementById("email-input");
 
     nextButton.addEventListener("click", function () {
         const email = emailInput.value.trim();
+        
         if (isValidEmail(email)) {
             const user = getUserData(email);
+            
             if (user) {
                 if (user.isAdmin) {
                     // Redirect to main.html if the email is the admin's email
@@ -19,29 +24,16 @@ document.addEventListener("DOMContentLoaded", function () {
                     window.location.href = "pay.html";
                 }
             } else {
-                // Handle non-existing users
-                alert("User not found. Please sign up or check your email.");
+                // Redirect to payment.html for non-existing users
+                window.location.href = "pay.html";
             }
         } else {
             alert("Invalid email address. Please enter a valid email.");
         }
     });
 
-    // Function to validate an email address
+    // Function to validate an email address (you can use a more robust validation method)
     function isValidEmail(email) {
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    }
-
-    // Sample user information (replace with your actual user data)
-    const users = [
-        { email: 'familyvatistas90@gmail.com', isAdmin: true, hasPaid: true },
-        { email: 'user2@example.com', isAdmin: false, hasPaid: true },
-        { email: 'user3@example.com', isAdmin: false, hasPaid: false },
-        // Add more user data as needed
-    ];
-
-    // Function to check user credentials and return user data
-    function getUserData(email) {
-        return users.find(user => user.email === email);
     }
 });
