edc46638b0932c78c485975afe2d31fb2834db83
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-16 23:26:57 +0300

    Update pay.js

--- pay.js
+++ pay.js
@@ -1,22 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    paypal.Buttons({
-        createOrder: function (data, actions) {
-            return actions.order.create({
-                purchase_units: [{
-                    amount: {
-                        value: '50.00' // Adjust the payment amount as needed
-                    }
-                }]
-            });
-        },
-        onApprove: function (data, actions) {
-            // Capture the funds from the transaction
-            return actions.order.capture().then(function (details) {
-                // Payment successful
-                console.log('Transaction completed by ' + details.payer.name.given_name);
-                // Redirect to a success page or perform other actions
-                window.location.href = "main.html";
-            });
-        }
-    }).render('#paypal-button');
+    const paypalButton = document.getElementById("paypal-button");
+
+    paypalButton.addEventListener("click", function () {
+        // Replace 'YOUR_CLIENT_ID' with your actual PayPal client ID
+        const clientId = 'AU_rP43t9nXsK1lrVxTpdoadLM19m4TodwP3O1A2586msFhIMF-bCMYkV2IlE5lXzomty6gWW_v29pNv';
+        const amount = '50.00'; // Adjust the payment amount as needed
+        const returnUrl = 'https://dimvat1.github.io/aichat/main.html'; // Replace with your success page URL
+        const cancelUrl = 'https://dimvat1.github.io/aichat/'; // Replace with your cancel page URL
+
+        // Construct the PayPal payment link
+        const paypalLink = `https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=${clientId}&item_name=Payment&amount=${amount}&return=${returnUrl}&cancel_return=${cancelUrl}`;
+
+        // Open a new window or modal dialog with the PayPal link
+        window.open(paypalLink, 'PayPal Payment', 'width=600,height=400');
+    });
 });
