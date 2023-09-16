26ac4bbce0c86a8fa35dd4ee0b5c98fa01aebf3e
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-16 15:37:35 +0300

    Update info.js

--- info.js
+++ info.js
@@ -1,9 +1,16 @@
 // Sample user information (replace with your actual user data)
 const users = [
-    { email: 'user1@example.com', password: 'password1', paid: true },
+    { email: 'user@example.com', password: 'password', paid: true },
     { email: 'user2@example.com', password: 'password2', paid: false },
     // Add more user data as needed
 ];
 
-// Export the user data
-export { users };
+// Function to check user credentials and return user data
+function getUserData(email, password) {
+    return users.find(user => user.email === email && user.password === password);
+}
+
+// Export the getUserData function (you can add more functions as needed)
+module.exports = {
+    getUserData
+};
