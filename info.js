66c89eff010fa4bf8b5d5a9bcc3ca151adae1eac
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-16 22:48:37 +0300

    Update info.js

--- info.js
+++ info.js
@@ -1,14 +1,11 @@
-// Sample user information (replace with your actual user data)
 const users = [
-    { email: 'familyvatistas90@gmail.com', paid: true },
+    { email: 'user@example.com', paid: true },
     { email: 'user2@example.com', paid: false },
     // Add more user data as needed
 ];
 
-// Function to check user payment status and return user data
 function getUserData(email) {
     return users.find(user => user.email === email);
 }
 
-// Export the getUserData function (you can add more functions as needed)
 export { getUserData };
