2ade3644766ab7687d1a1e61d08edf67444b758c
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-18 23:09:01 +0300

    Update openai-image-generation.js

--- openai-image-generation.js
+++ openai-image-generation.js
@@ -1,43 +1,64 @@
+// Import OpenAI library
+import OpenAI from "openai";
+
 // Replace 'your-api-key' with your actual OpenAI API key
 const apiKey = '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';
+
+// Initialize OpenAI instance
+const openai = new OpenAI({ apiKey });
 
 document.addEventListener("DOMContentLoaded", function () {
     const userInput = document.getElementById("user-input");
     const imageButton = document.getElementById("image-button");
     const chatBox = document.getElementById("chat-box");
     const downloadButton = document.getElementById("download-button");
+    const generatedImage = document.getElementById("generated-image"); // Reference to the <img> element
 
     imageButton.addEventListener("click", generateImage);
-    downloadButton.addEventListener("click", downloadImage);
+    downloadButton.style.display = "none"; // Hide the "Download" button initially
 
-    function generateImage() {
+    async function generateImage() {
         const userPrompt = userInput.value.trim();
         if (userPrompt !== "") {
-            axios.post('https://api.openai.com/v1/davinci/images/generate', {
-                prompt: userPrompt,
-            }, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${apiKey}`,
-                },
-            })
-            .then(function (response) {
-                const generatedImageData = response.data; // This will contain the generated image data
-                // You can display the image or manipulate it as needed
-                // For example, you can create an <img> element and set its 'src' attribute with the image data
+            try {
+                const response = await openai.images.generate({ prompt: userPrompt });
+                const imageUrls = response.data.map((imageData) => imageData.url);
+                if (imageUrls.length > 0) {
+                    // Display the generated image by setting the 'src' attribute of the <img> element
+                    const generatedImageUrl = imageUrls[0];
+                    generatedImage.src = generatedImageUrl; // Set the 'src' attribute
 
-                // Show the "Download Image" button
-                downloadButton.style.display = "inline";
-            })
-            .catch(function (error) {
+                    // Show the "Download" button
+                    downloadButton.style.display = "inline";
+                } else {
+                    console.error("No image URL received from OpenAI.");
+                    appendMessage("AI Chatbot", "An error occurred while generating the image.");
+                }
+            } catch (error) {
                 console.error(error);
                 appendMessage("AI Chatbot", "An error occurred while generating the image.");
-            });
+            }
         }
     }
 
+  downloadButton.addEventListener("click", downloadImage);
+
     function downloadImage() {
-        // You can implement the image download logic here if needed
-        // Retrieve the generated image data and save it as a file
+        // Retrieve the image URL from the "data-image-url" attribute
+        const imageUrl = downloadButton.dataset.imageUrl;
+        if (imageUrl) {
+            // Create an anchor element to trigger the download
+            const downloadLink = document.createElement("a");
+            downloadLink.href = imageUrl;
+            downloadLink.download = "generated-image.png"; // Specify the desired file name
+            downloadLink.style.display = "none";
+            document.body.appendChild(downloadLink);
+
+            // Trigger the click event to start the download
+            downloadLink.click();
+
+            // Clean up
+            document.body.removeChild(downloadLink);
+        }
     }
 });
