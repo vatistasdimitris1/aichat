33e442ba15e4ddc3ce980bb436cb0f808212bc79
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-20 00:18:26 +0300

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,51 @@
+import { getUsers, addUser, removeUser } from './info.js';
+
+document.addEventListener("DOMContentLoaded", function () {
+    const userList = document.getElementById("user-list");
+    const newUserInput = document.getElementById("new-email");
+    const addUserButton = document.getElementById("add-user");
+
+    // Load initial user data
+    loadUsers();
+
+    // Load user data and populate the table
+    function loadUsers() {
+        userList.innerHTML = ""; // Clear the existing user list
+        const users = getUsers();
+
+        users.forEach(user => {
+            const row = document.createElement("tr");
+            const emailCell = document.createElement("td");
+            const paidCell = document.createElement("td");
+            const actionsCell = document.createElement("td");
+            const removeButton = document.createElement("button");
+
+            emailCell.textContent = user.email;
+            paidCell.textContent = user.paid ? "Yes" : "No";
+
+            removeButton.textContent = "Remove";
+            removeButton.addEventListener("click", () => {
+                removeUser(user.email);
+                loadUsers(); // Reload the user list after removal
+            });
+
+            actionsCell.appendChild(removeButton);
+
+            row.appendChild(emailCell);
+            row.appendChild(paidCell);
+            row.appendChild(actionsCell);
+
+            userList.appendChild(row);
+        });
+    }
+
+    // Add a new user
+    addUserButton.addEventListener("click", () => {
+        const newEmail = newUserInput.value.trim();
+        if (newEmail) {
+            addUser({ email: newEmail, paid: false });
+            newUserInput.value = ""; // Clear the input
+            loadUsers(); // Reload the user list after addition
+        }
+    });
+});
