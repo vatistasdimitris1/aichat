227d2c76b6200398824db7bd1a51486de89b67a7
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-21 18:16:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,45 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const querystring = require('querystring');
+const { getUsers } = require('./info.js'); // Import getUsers function
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST' && req.url === '/save-data') {
+        // ... (existing code)
+
+        // Save to a text file (e.g., info.txt)
+        fs.appendFile('info.txt', `${email},${isPaid}\n`, (err) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end('Data saved successfully');
+            }
+        });
+    } else if (req.url === '/get-users') {
+        // Serve the user data from info.js as JSON
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(getUsers()));
+    } else if (req.url === '/info.txt') {
+        // Serve the info.txt file
+        fs.readFile('info.txt', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
 
 const server = http.createServer((req, res) => {
   if (req.method === 'POST' && req.url === '/save-data') {
