a6dda6af17d6e57d86d2f9f82524b10365dc6395
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-21 19:14:07 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,89 +1,47 @@
-$(document).ready(function () {
-    const newUserInput = $("#new-email");
-    const userList = $("#userList");
+import { getUsers, addUser, removeUser } from './info.js';
 
-    // Function to load users from the server and display them
-    function loadUsers() {
-        $.ajax({
-            type: "GET",
-            url: "/get-users", // Adjust the URL as needed
-            dataType: "json",
-            success: function (data) {
-                userList.empty(); // Clear the user list
-                data.forEach(function (user) {
-                    userList.append(`<tr data-email="${user.email}">
-                        <td>${user.email}</td>
-                        <td>${user.paid ? "Yes" : "No"}</td>
-                        <td>
-                            <button class="edit-user">Edit</button>
-                            <button class="remove-user">Remove</button>
-                        </td>
-                    </tr>`);
-                });
-            },
-            error: function (err) {
-                console.error("Error loading users:", err);
-            }
+document.addEventListener('DOMContentLoaded', function () {
+    const addUserForm = document.getElementById('addUserForm');
+    const newUserEmail = document.getElementById('newUserEmail');
+    const newUserPaid = document.getElementById('newUserPaid');
+    const userList = document.getElementById('userList');
+
+    // Function to update the user list
+    function updateUsers() {
+        userList.innerHTML = '';
+        const users = getUsers();
+
+        users.forEach(user => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `${user.email} (Paid: ${user.paid ? 'Yes' : 'No'})`;
+            userList.appendChild(listItem);
         });
     }
 
-    // Load users when the page loads
-    loadUsers();
-
     // Event listener to add a new user
-    $("#add-user").click(function () {
-        const newEmail = newUserInput.val().trim();
-        if (newEmail) {
-            $.ajax({
-                type: "POST",
-                url: "/add-user", // Adjust the URL as needed
-                data: { email: newEmail },
-                success: function () {
-                    newUserInput.val(""); // Clear the input
-                    loadUsers(); // Reload the user list after addition
-                },
-                error: function (err) {
-                    console.error("Error adding user:", err);
-                }
-            });
+    addUserForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const email = newUserEmail.value.trim();
+        const isPaid = newUserPaid.checked;
+
+        if (email) {
+            addUser({ email, paid: isPaid });
+            updateUsers();
+            newUserEmail.value = '';
+            newUserPaid.checked = false;
         }
     });
 
-    // Event listener to remove a user
-    userList.on("click", ".remove-user", function () {
-        const email = $(this).closest("tr").data("email");
-        if (email) {
-            $.ajax({
-                type: "POST",
-                url: "/remove-user", // Adjust the URL as needed
-                data: { email: email },
-                success: function () {
-                    loadUsers(); // Reload the user list after removal
-                },
-                error: function (err) {
-                    console.error("Error removing user:", err);
-                }
-            });
+    // Event listener to remove a user (you can add more logic here)
+    userList.addEventListener('click', function (e) {
+        if (e.target && e.target.nodeName === 'LI') {
+            const userEmail = e.target.textContent.split(' ')[0];
+            removeUser(userEmail);
+            updateUsers();
         }
     });
 
-    // Event listener to edit a user's payment status
-    userList.on("click", ".edit-user", function () {
-        const email = $(this).closest("tr").data("email");
-        const row = $(this).closest("tr");
-        if (email) {
-            const isPaid = row.find("td:eq(1)").text() === "Yes" ? false : true;
-            $.ajax({
-                type: "POST",
-                url: "/edit-user", // Adjust the URL as needed
-                data: { email: email, paid: isPaid },
-                success: function () {
-                    loadUsers(); // Reload the user list after editing
-                },
-                error: function (err) {
-                    console.error("Error editing user:", err);
-                }
-            });
-        }
-    });
+    // Initial user list update
+    updateUsers();
 });
