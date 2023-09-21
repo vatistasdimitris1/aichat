9a6944eec8e05691a42e7314c88f4efe01e63df3
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-21 18:59:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-// Import functions from info.js
-const { getUserData, saveEmailAndPaymentStatus, getUsers, addUser, removeUser } = require('./info');
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Serve static files
+// Serve static files (HTML, CSS, JavaScript)
 app.use(express.static('public'));
 
-// Route to get all users
-app.get("/get-users", (req, res) => {
-    const usersData = getUsers();
-    res.json(usersData);
+// Sample user data (replace with your data)
+let users = [
+    { email: 'user@example.com', paid: false },
+    { email: 'familyvatistas90@gmail.com', paid: true },
+];
+
+// Get all users
+app.get('/get-users', (req, res) => {
+    res.json(users);
 });
 
-// Route to add a new user
-app.post("/add-user", (req, res) => {
-    const { email, paid } = req.body;
-    addUser({ email, paid });
-    res.send("User added successfully");
-});
-
-// Route to toggle payment status
-app.post("/toggle-payment", (req, res) => {
-    const email = req.body.email;
-    const user = getUserData(email);
-    if (user) {
-        saveEmailAndPaymentStatus(email, !user.paid);
-        res.send("Payment status toggled successfully");
-    } else {
-        res.status(404).send("User not found");
-    }
-});
-
-// Route to remove a user
-app.post("/remove-user", (req, res) => {
-    const email = req.body.email;
-    removeUser(email);
-    res.send("User removed successfully");
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// Add a new user
+app.post('/add-user', (
