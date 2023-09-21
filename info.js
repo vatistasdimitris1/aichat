feef402fc890a959c7eb6ab4113d6544e03db25b
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-21 18:32:20 +0300

    Update info.js

--- info.js
+++ info.js
@@ -1,4 +1,43 @@
-// info.js
+// Sample user data (replace with your data)
+const users = [
+    { email: 'user@example.com', paid: false },
+    { email: 'familyvatistas90@gmail.com', paid: true },
+    { email: 'DimVat', paid: true }
+    // Add more user data as needed
+];
+
+// Function to get user data by email
+function getUserData(email) {
+    return users.find(user => user.email === email);
+}
+
+// Function to save email and payment status to info.js
+function saveEmailAndPaymentStatus(email, isPaid) {
+    const user = getUserData(email);
+
+    if (user) {
+        user.email = email; // Update the user's email
+        user.paid = isPaid; // Update the payment status
+    }
+}
+
+// Function to get all users
+function getUsers() {
+    return users;
+}
+
+// Function to add a new user
+function addUser(newUser) {
+    users.push(newUser);
+}
+
+// Function to remove a user by email
+function removeUser(email) {
+    const index = users.findIndex(user => user.email === email);
+    if (index !== -1) {
+        users.splice(index, 1);
+    }
+}
 
 $(document).ready(function () {
     const addUserButton = $("#addUserButton");
