bd8f3fcd7e4fce122a39839e93d1555f894cd026
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-09-21 19:21:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,78 @@
 const express = require('express');
-const fs = require('fs');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
 const app = express();
 const port = 3000;
 
-// Middleware to parse JSON request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Read the initial user data from 'users.json' or create an empty array
-let users = [];
-if (fs.existsSync('users.json')) {
-    const rawData = fs.readFileSync('users.json');
-    users = JSON.parse(rawData);
-}
+// Serve static files (HTML, CSS, JS)
+app.use(express.static('public'));
 
-// Function to save email and payment status to users list and info.txt
-function saveEmailAndPaymentStatus(email, isPaid) {
-    const user = getUserData(email);
-
-    if (user) {
-        user.email = email; // Update the user's email
-        user.paid = isPaid; // Update the payment status
-    } else {
-        // If the user does not exist, add a new user
-        users.push({ email, paid: isPaid });
-    }
-
-    // Save the updated user list to a file (users.json)
-    fs.writeFileSync('users.json', JSON.stringify(users, null, 2));
-
-    // Save the updated user list to 'info.txt'
-    const userLines = users.map(user => `${user.email},Paid:${user.paid ? 'true' : 'false'}`);
-    fs.writeFileSync('info.txt', userLines.join('\n'));
-}
+// Sample user data (replace with your data)
+const users = [
+    { email: 'user@example.com', paid: false },
+    { email: 'familyvatistas90@gmail.com', paid: true },
+    { email: 'DimVat', paid: true }
+    // Add more user data as needed
+];
 
 // Function to get user data by email
 function getUserData(email) {
     return users.find(user => user.email === email);
 }
 
-// Route to get the list of users
+// Function to save email and payment status to info.js
+function saveEmailAndPaymentStatus(email, isPaid) {
+    const user = getUserData(email);
+
+    if (user) {
+        user.email = email; // Update the user's email
+        user.paid = isPaid; // Update the payment status
+    }
+}
+
+// Function to get all users
+function getUsers() {
+    return users;
+}
+
+// Function to add a new user
+function addUser(newUser) {
+    users.push(newUser);
+}
+
+// Function to remove a user by email
+function removeUser(email) {
+    const index = users.findIndex(user => user.email === email);
+    if (index !== -1) {
+        users.splice(index, 1);
+    }
+}
+
+// Route to get users
 app.get('/get-users', (req, res) => {
-    res.json(users);
+    res.json(getUsers());
 });
 
-// Route to handle user signups
-app.post('/signup', (req, res) => {
+// Route to save user data
+app.post('/save-data', (req, res) => {
     const { email, isPaid } = req.body;
+
+    // Save data to info.js
     saveEmailAndPaymentStatus(email, isPaid);
-    res.sendStatus(200); // Send a success status
+
+    // Optionally, save data to info.txt
+    const userData = `${email} (Paid: ${isPaid ? 'Yes' : 'No'})\n`;
+    fs.appendFile('info.txt', userData, (err) => {
+        if (err) {
+            console.error('Error saving data to info.txt:', err);
+        }
+    });
+
+    res.sendStatus(200);
 });
 
 app.listen(port, () => {
